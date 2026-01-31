9358ad4934e2420f5c425e13c12ee2481f566c1a
Aayusman Mallick <aayusmanm@gmail.com>
2026-02-01 02:31:52 +0530

    Fix JSON syntax and remove legacy deps

--- postcss.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [
-    require('postcss-easy-import')(),
-    require('postcss-custom-properties')({ preserve: false }),
-    require('postcss-color-function')(),
-    require('autoprefixer')(),
-  ],
-}
